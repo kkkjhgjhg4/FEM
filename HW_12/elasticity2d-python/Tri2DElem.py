cae45e30185d74cc3a618200e9f88822d349f06e
黄炳棋 <huangbingqi@Bingqis-Macbook-Air.local>
2024-04-26 16:42:26 +0800

    add the 2d Triangle element

---
+++ HW_12/elasticity2d-python/Tri2DElem.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+import FEData as model
+from utitls import gauss
+import numpy as np
+
+
+def Tri2DElem(e):
+	"""
+	Calculate element stiffness matrix and element nodal body force vector
+
+	Args:
+		e : (int) element number
+
+	Returns: ke, fe
+		ke : (numpy(nen,nen)) element stiffness matrix
+		fe : (numpy(nen,1)) element nodal force vector
+	"""
+	ke = np.zeros((model.nen*model.ndof, model.nen*model.ndof))
+	fe = np.zeros((model.nen*model.ndof, 1))
+
+	# get coordinates of element nodes
+	je = model.IEN[:, e] - 1
+	C = np.array([model.x[je], model.y[je]]).T
+	a, b, c, Ae=ParamCalc(C)
+  
+	# derivative of shape function
+	B = BmatTri2D(C)
+	ke = Ae * B.T @ model.D @ B
+	
+	# compute element nodal force vector
+	fe = Ae @ model.b[:, e].reshape((-1, 1))
+	fe = fe / 3
+	
+	return ke, fe
+
+
+def NmatTri2D(x, y, C):
+	"""
+	Calculate element shape function matrix N at coordinate xt
+
+	Args:
+		Ae: Area of the element in physical coordinates
+		
+
+	Returns:
+		Element shape function matrix N
+	"""
+	
+	a, b, c, Ae = ParamCalc(C)
+
+	N1 = (a[0] + b[0] * x + c[0] * y) / (2 * Ae)
+	N2 = (a[1] + b[1] * x + c[1] * y) / (2 * Ae)
+	N3 = (a[2] + b[2] * x + c[2] * y) / (2 * Ae)
+
+	return np.array([[N1, 0, N2, 0, N3, 0],
+					 [0, N1, 0, N2, 0, N3]])
+
+
+def BmatTri2D(C):
+	"""
+	Calcualte derivative of element shape function matrix B at coordinate xt
+
+	Args:
+		C   : The physical coordinates
+
+	Returns:
+		Derivative of element shape function matrix B and Jacobian determination
+	"""
+	a, b, c, Ae = ParamCalc(C)
+	
+	B = np.array([[b[0], 0, b[1], 0, b[2], 0],
+				  [0, c[0], 0, c[1], 0, c[2]],
+				  [c[0], b[0], c[1], b[1], c[2], b[2]]]) / (2*Ae)
+
+	return B
+
+def DNmatTri2D(eta, psi, C):
+	"""
+	Calcualte derivative of element shape function matrix DN at coordinate xt
+
+	Args:
+		eta : The first parent coordinate
+		psi : The second parent coordinate
+		C   : The physical coordinates
+
+	Return:
+		Derivative of element shape function matrix BB
+	"""
+
+	#Calculate the Grad(N) matrix in parent coordinate
+	GN = 0.25*np.array([[eta-1, 1-eta, 1+eta, -eta-1],
+						[psi-1, -psi-1, 1+psi, 1-psi]])
+
+	# Compute Jacobian matrix
+	J = GN@C
+
+	DN = np.linalg.solve(J, GN)
+
+	return DN
+
+
+def ParamCalc(C):
+	'''
+	Calculate a b and c in Shape function N and B
+
+	Args:
+		C: coordinate vetor of the element in physical coordnates
+	'''
+	a = np.zeros[3]
+	b = np.zeros[3]
+	c = np.zeros[3]
+	
+	a[0]=C[0][1]-C[1][2]
+	a[1]=C[0][2]-C[1][0]
+	a[2]=C[0][0]-C[1][1]
+
+	b[0]=C[1][1]-C[1][2]
+	b[1]=C[1][2]-C[1][0]
+	b[2]=C[1][0]-C[1][1]
+
+	c[0]=C[0][2]-C[0][1]
+	c[0]=C[0][0]-C[0][2]
+	c[0]=C[0][1]-C[0][0]
+	
+	Ae=0.5 * np.sum(a)
+
+	return a, b, c, Ae
+
